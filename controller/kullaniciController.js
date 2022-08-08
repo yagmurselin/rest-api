3c199cbf4c00c01cdae856a006ec3029bf9b9178
yagmur selin <yelin.senturk@gmail.com>
2022-08-08 11:44:35 +0300

    put, push, delete, update metotlatı kusursuz çalışıyor.

--- controller/kullaniciController.js
+++ controller/kullaniciController.js
@@ -1,12 +1,12 @@
-const Kullanici = require("../models/kullaniciModel");
+var Kullanici = require("../models/kullaniciModel");
 const { getPostData } = require("../utils");
 
 async function getKullanicilar(req, res) {
   try {
-    const kullaicilar = await Kullanici.findAll();
+    const kullanicilar = await Kullanici.findAll();
 
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(kullaicilar));
+    res.end(JSON.stringify(kullanicilar));
   } catch (error) {
     console.log(error);
   }
@@ -42,8 +42,51 @@ async function createKullanici(req, res) {
   }
 }
 
+async function updateKullanici(req, res, id) {
+  try {
+    const kullanici = await Kullanici.findByID(id);
+
+    if (!kullanici) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Kullanıcı bulunamadı" }));
+    } else {
+      const body = await getPostData(req);
+      const { isim, email } = JSON.parse(body);
+
+      const kullaniciVerisi = {
+        isim: isim || kullanici.isim,
+        email: email || kullanici.email,
+      };
+      const updateKullanici = await Kullanici.update(id, kullaniciVerisi);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(updateKullanici));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function deleteKullanici(req, res, id) {
+  try {
+    const kullanici = await Kullanici.findByID(id);
+
+    if (!kullanici) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Kullanıcı bulunamadı" }));
+    } else {
+      await Kullanici.remove(id);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ mesaj: `Kullanici id=${id} silindi` }));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports = {
   getKullanicilar,
   getKullanici,
   createKullanici,
+  updateKullanici,
+  deleteKullanici,
 };
